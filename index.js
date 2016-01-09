b71e93cd9e246371aeef8d321cf13009ec3372ef
David Yeung <quxbaz@gmail.com>
2016-01-09 15:08:07 -0500

    modified:   index.js

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ import Sequencer from './lib/sequencer';
 import Channel from './lib/channel';
 import Blip from './lib/blip';
 import AudioService from './lib/audioservice';
-export {Sequencer, Channel, Blip, AudioService};
+export {
+  Sequencer,
+  Channel,
+  Blip,
+  AudioService
+};
