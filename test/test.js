296dd74e4ff207addc1ed378e28cea4d17833147
David Yeung <quxbaz@gmail.com>
2016-01-10 02:34:47 -0500

    mixin now triggers handlers on state changes.

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 require('node_modules/chai/index.js').should();
+import './test_stateful';
 import './test_sequencer';
 import './test_channel';
 import './test_blip';
---
+++ test/test_stateful.js
@@ -0,0 +1,49 @@
+import stateful from 'lib/stateful';
+
+describe('Stateful mixin', function() {
+
+  let Foo;
+  let foo;
+
+  beforeEach(() => {
+    Foo = function(){};
+    Object.assign(Foo.prototype, stateful.mixin);
+    foo = new Foo();
+  });
+
+  it("calls setState()", () => {
+    foo.setState({a:1, b:2});
+    foo.state.a.should.eql(1);
+    foo.state.b.should.eql(2);
+  });
+
+  it("performs an overwrite with setState()", () => {
+    foo.setState({a: 1});
+    foo.state.a.should.eql(1);
+    foo.setState({a: 2});
+    foo.state.a.should.eql(2);
+  });
+
+  it("triggers a callback on change state.", () => {
+    let i = 0;
+    foo.onChangeState((newState) => {
+      i++;
+    });
+    foo.setState({a: 1});
+    i.should.eql(1);
+    foo.setState({a: 2});
+    i.should.eql(2);
+  });
+
+  it("does not trigger a callback on calling setState() with the same state.", function() {
+    let i = 0;
+    foo.onChangeState((newState) => {
+      i++;
+    });
+    foo.setState({a: 1});
+    i.should.eql(1);
+    foo.setState({a: 1});
+    i.should.eql(1);
+  });
+
+});
