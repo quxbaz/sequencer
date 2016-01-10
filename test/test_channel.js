37581bb140bdc6a8892b0e8074c3be21874b684a
David Yeung <quxbaz@gmail.com>
2016-01-10 18:00:11 -0500

    Channel default blips are now mute.

--- test/test_channel.js
+++ test/test_channel.js
@@ -8,7 +8,13 @@ describe('Channel', () => {
     channel = new Channel();
   });
 
-  it("creates Blip objects with the same @sampleName state as its own.", () => {
+  it("creates default Blip objects that are mute.", () => {
+    channel.state.blips.forEach((blip) => {
+      blip.state.mute.should.be.true;
+    });
+  });
+
+  it("creates default Blip objects with the same @sampleName state as its own.", () => {
     let channel = new Channel({sampleName: 'kick'});
     channel.state.blips.forEach((blip) => {
       blip.state.sampleName.should.eql('kick');
