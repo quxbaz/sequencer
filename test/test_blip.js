f728bdc7b02d1542df4ddefdda9125e31cc9cdc5
David Yeung <quxbaz@gmail.com>
2016-01-10 21:29:25 -0500

    Now suports computed states.

--- test/test_blip.js
+++ test/test_blip.js
@@ -1,6 +1,6 @@
 import Blip from 'lib/blip';
 
-describe('Blip', function() {
+describe('Blip', () => {
 
   let blip;
 
@@ -8,12 +8,12 @@ describe('Blip', function() {
     blip = new Blip();
   });
 
-  it("sets gain.", function() {
+  it("sets gain.", () => {
     blip.setState({gain: 2});
     blip.state.gain.should.eql(2);
   });
 
-  it("sets rate.", function() {
+  it("sets rate.", () => {
     blip.setState({rate: 2});
     blip.state.rate.should.eql(2);
   });
@@ -37,5 +37,32 @@ describe('Blip', function() {
     blip.setState({gain: -1});
     blip.state.gain.should.eql(blip.state.minGain);
   });
+
+  it("sets a new max gain and validates again it.", () => {
+    blip.setState({gain: 99, maxGain: 200});
+    blip.state.gain.should.eql(99);
+  });
+
+  it("sets a new min gain and validates again it.", () => {
+    blip.setState({gain: -2, minGain: -3});
+    blip.state.gain.should.eql(-2);
+  });
+
+  it("gainScale returns 1 on setting gain to max.", () => {
+    blip.setState({gain: blip.state.maxGain});
+    blip.state.gainScale.should.eql(1);
+  });
+
+  it("gainScale returns 0 on setting gain to min.", () => {
+    blip.setState({gain: blip.state.minGain});
+    blip.state.gainScale.should.eql(0);
+  });
+
+  it("gainScale returns 0.5 on setting gain to its middle.", () => {
+    let middle = (blip.state.maxGain + blip.state.minGain) / 2;
+    blip.setState({gain: middle});
+    blip.state.gainScale.should.eql(0.5);
+  });
+
 
 });
