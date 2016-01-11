87d498ee0c19b282d697bbf6189bc52925429d1b
David Yeung <quxbaz@gmail.com>
2016-01-10 20:21:02 -0500

    Blip now validates min/max props. Changed playbackRate -> rate.

--- test/test_blip.js
+++ test/test_blip.js
@@ -2,8 +2,40 @@ import Blip from 'lib/blip';
 
 describe('Blip', function() {
 
-  it("creates a Blip object.", () => {
-    new Blip();
+  let blip;
+
+  beforeEach(() => {
+    blip = new Blip();
+  });
+
+  it("sets gain.", function() {
+    blip.setState({gain: 2});
+    blip.state.gain.should.eql(2);
+  });
+
+  it("sets rate.", function() {
+    blip.setState({rate: 2});
+    blip.state.rate.should.eql(2);
+  });
+
+  it("enforces a max rate.", () => {
+    blip.setState({rate: 99});
+    blip.state.rate.should.eql(blip.state.maxRate);
+  });
+
+  it("enforces a min rate.", () => {
+    blip.setState({rate: -1});
+    blip.state.rate.should.eql(blip.state.minRate);
+  });
+
+  it("enforces a max gain.", () => {
+    blip.setState({gain: 99});
+    blip.state.gain.should.eql(blip.state.maxGain);
+  });
+
+  it("enforces a min gain.", () => {
+    blip.setState({gain: -1});
+    blip.state.gain.should.eql(blip.state.minGain);
   });
 
 });
